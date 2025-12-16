f44eaf2f1d18065e5f1e8b74bdc53b2e13f552d6
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-16 15:13:44 +0100

    feat: implemento risposta json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import comicPosts from "./posts.js";
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,14 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Server del mio blog</h1>");
+});
+
+app.get("/bacheca", (req, res) => {
+  const response = {
+    postTotali: comicPosts.length,
+    ListaPost: comicPosts,
+  };
+  res.json(response);
 });
 
 app.listen(port, () => {
