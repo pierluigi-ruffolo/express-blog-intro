b827793d3f33435e9addb12634fa993bddf1fa0e
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-16 14:14:42 +0100

    feat: creo rotta /  che ritorna un testo semplice con scritto ”Server del mio blog”

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>Server del mio blog</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`server avviato sulla porta ${port}`);
+});
