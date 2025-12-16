ea3adadf7e1ea8d67e313dfc8d26f63d10360948
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-16 14:31:49 +0100

    feat: creo array inserisco nel public img

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("<h1>Server del mio blog</h1>");
 });
